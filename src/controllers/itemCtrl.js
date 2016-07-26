3ea9ef02fb9c61aef85bf6eed5cd9de949ffca0d
natereinhardt <natemreinhardt@gmail.com>
2016-07-26 07:10:14 -0500

    change some stuff up

--- src/controllers/itemCtrl.js
+++ src/controllers/itemCtrl.js
@@ -8,78 +8,9 @@ var itemModel = require('./../models/itemModel');
 
 var eveItemCrestEndpoint = 'https://crest-tq.eveonline.com/inventory/types/';
 
-/**
- *
- ****ORE****
- *itemType: 1
- 22          Arkonor
- 17425       Crimson Arkonor
- 17426       Prime Arkonor
- 1223        Bistot
- 17428       Triclinic Bistot
- 17429       Monoclinic Bistot
- 1225        Crokite
- 17432       Sharp Crokite
- 17433       Crystalline Crokite
- 1232        Dark Ochre
- 17436       Onyx Ochre
- 17437       Obsidian Ochre
- 21          Hedbergite
- 17440       Vitric Hedbergite
- 17441       Glazed Hedbergite
- 1231        Hemorphite
- 17444       Vivid Hemorphite
- 17445       Radiant Hemorphite
- 1226        Jaspet
- 17448       Pure Jaspet
- 17449       Pristine Jaspet
- 20          Kernite
- 17452       Luminous Kernite
- 17453       Fiery Kernite
- 18          Plagioclase
- 17455       Azure Plagioclase
- 17456       Rich Plagioclase
- 1224        Pyroxeres
- 17459       Solid Pyroxeres
- 17460       Viscous Pyroxeres
- 1228        Scordite
- 17463       Condensed Scordite
- 17464       Massive Scordite
- 19          Spodumain
- 17466       Bright Spodumain
- 17467       Gleaming Spodumain
- 1230        Veldspar
- 17470       Concentrated Veldspar
- 17471       Dense Veldspar
- 1229        Gneiss
- 17865       Iridescent Gneiss
- 17866       Prismatic Gneiss
- 11396       Mercoxit
- 17869       Magma Mercoxit
- 17870       Vitreous Mercoxit
- 1227        Omber
- 17867       Silvery Omber
- 17868       Golden Omber
- *
- *
- *
- * ****ICE****
- *itemType: 2
- 16262       Clear Icicle
- 17978       Enriched Clear Icicle
- 16263       Glacial Mass
- 17977       Smooth Glacial Mass
- 16264       Blue Ice
- 17975       Thick Blue Ice
- 16265       White Glaze
- 17976       Pristine White Glaze
- 16266       Glare Crust
- 16267       Dark Glitter
- *
- */
-
+//ORE : 1
 var oreIds = [
-    //ORE
+
     '22' ,    // Arkonor
     '17425',  // Crimson Arkonor
     '17426',  // Prime Arkonor
@@ -129,9 +60,8 @@ var oreIds = [
     '17867',  // Silvery Omber
     '17868'  // Golden Omber
 ];
-
+//ICE - 2
 var iceIds = [
-    //ICE
     '16262', //   Clear Icicle
     '17978', //   Enriched Clear Icicle
     '16263', //   Glacial Mass
--- src/controllers/shipCtrl.js
+++ src/controllers/shipCtrl.js
@@ -7,80 +7,7 @@ var shipModel = require('./../models/shipModel');
 
 
 var eveItemCrestEndpoint = 'https://crest-tq.eveonline.com/inventory/types/';
-
 /**
- 1 - Mining Barge
-
- 17476       Covetor
- 17478       Retriever
- 17480       Procurer
-
- 2 - Exhumer
-
- 22548       Mackinaw
- 22546       Skiff
- 22544       Hulk
-
- 3 - Mining Frigate
-
- 32880       Venture
- 33697       Prospect
- 353428      Endurance
-
- 4 - Industrial Command Ship
-
- 28606       Orca
- 28352       Rorqual
-
- 5 - Hauler
-
- ~AMAR
- 1944        Bestower
- 19744       Sigil
- 12733       Prorator
- 12753       Impel
- 20183       Providence
- 28850       Ark
- ~CALDARI
- 648         Badger
- 649         Tayra
- 12729       Crane
- 12731       Bustard
- 20185       Charon
- 28844       Rhea
- ~GALLENTE
- 650         Nereus
- 654         Kryos
- 655         Epithal
- 656         Miasmos
- 657         Iteron Mark V
- 12743       Viator
- 12745       Occator
- 20187       Obelisk
- 28848       Anshar
-
- ~MINMATAR
- 651         Hoarder
- 652         Mammoth
- 653         Wreathe
- 12735       Prowler
- 12747       Mastodon
- 20189       Fenrir
- 28846       Nomad
-
-
-
- ships = [<item id of mining/hauling ships>]
- * 1) Call Item Types "https://crest-tq.eveonline.com/inventory/types/{shipIds}
- * ---- a) Create Ship in DB
- *          i) Set ship type (Number 1-3)
- *          1 - Mining Barge
- *          2 - Exhumer
- *          3 - Mining Frigate
- *          4 - Industrial Command Ship
- *          5 - Hauler
- *
- *
  * NOTE: Build imageUrls
  *
  * 1) imgUrlSml: https://imageserver.eveonline.com/Type/{typeId}_32.png
@@ -89,90 +16,95 @@ var eveItemCrestEndpoint = 'https://crest-tq.eveonline.com/inventory/types/';
  *
  *
  * */
+
+/**Mining Barges: 1 **/
 var miningBargeIds = [
     '17476',  // Covetor
     '17478',  // Retriever
     '17480'   // Procurer
 ];
-
+/**Exhumers: 2 **/
 var exhumerIds = [
     '22548',  //Mackinaw
     '22546',  // Skiff
     '22544'   // Hulk
 ];
-
+/**Mining Frigates: 3 **/
 var miningFrigateIds = [
     '32880',  // Venture
     '33697',  //Prospect
     '353428'  // Endurance
 ];
-
+/**Industrial Command Ships: 4 **/
 var industrialCommandShipIds = [
     '28606',  //Orca
     '28352'   //Rorqual
 ];
-
+/**Haulers: 5 **/
 var haulerShipIds = [
     //~AMARR
-   '1944',     //Bestower
-   '19744',    //Sigil
-   '12733',    //Prorator
-   '12753',    //Impel
-   '20183',    //Providence
-   '28850',    //Ark
-   //~CALDARI
-   '648',      //Badger
-   '649',      //Tayra
-   '12729',    //Crane
-   '12731',    //Bustard
-   '20185',    //Charon
-   '28844',    //Rhea
-   //~GALLENTE
-   '650',      //Nereus
-   '654',      //Kryos
-   '655',      //Epithal
-   '656',      //Miasmos
-   '657',      //Iteron Mark V
-   '12743',    //Viator
-   '12745',    //Occator
-   '20187',    //Obelisk
-   '28848',    //Anshar
-   //~MINMATA
-   '651',      //Hoarder
-   '652',      //Mammoth
-   '653',      //Wreathe
-   '12735',    //Prowler
-   '12747',    //Mastodon
-   '20189',    //Fenrir
-   '28846'     //Nomad
+    '1944',     //Bestower
+    '19744',    //Sigil
+    '12733',    //Prorator
+    '12753',    //Impel
+    '20183',    //Providence
+    '28850',    //Ark
+    //~CALDARI
+    '648',      //Badger
+    '649',      //Tayra
+    '12729',    //Crane
+    '12731',    //Bustard
+    '20185',    //Charon
+    '28844',    //Rhea
+    //~GALLENTE
+    '650',      //Nereus
+    '654',      //Kryos
+    '655',      //Epithal
+    '656',      //Miasmos
+    '657',      //Iteron Mark V
+    '12743',    //Viator
+    '12745',    //Occator
+    '20187',    //Obelisk
+    '28848',    //Anshar
+    //~MINMATA
+    '651',      //Hoarder
+    '652',      //Mammoth
+    '653',      //Wreathe
+    '12735',    //Prowler
+    '12747',    //Mastodon
+    '20189',    //Fenrir
+    '28846'     //Nomad
 ];
+
 //TODO: Reduce Redundancy and Create an Update ALL Function
 exports.updateBargeData = function updateBargeData(req, res) {
     var ships = [];
     var allPromises = [];
-    for(var miningBargeId in miningBargeIds) {
+    for (var miningBargeId in miningBargeIds) {
         var options = {
-            uri: eveItemCrestEndpoint + miningBargeIds[miningBargeId] +'/',
+            uri: eveItemCrestEndpoint + miningBargeIds[miningBargeId] + '/',
             json: true
         };
         allPromises.push(RequestPromise(options));
     }
-    Promise.all(allPromises).then(function (responseItems){
-        for(var item in responseItems){
-            var newShip = new shipModel({
-                _id : responseItems[item].id,
-                name: responseItems[item].name,
-                description: responseItems[item].description,
-                itemType: 1,
-                imgUrlLrg: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_64.png',
-                imgUrlSml: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_32.png',
-                renderUrl: 'https://imageserver.eveonline.com/Render/'+responseItems[item].id+'_256.png'
-            });
-            newShip.save();
-            ships.push(newShip);
+    Promise.all(allPromises).then(function (responseItems) {
+        for (var item in responseItems) {
+            if (responseItems.hasOwnProperty(item)) {
+                var newShip = new shipModel({
+                    _id: responseItems[item].id,
+                    name: responseItems[item].name,
+                    description: responseItems[item].description,
+                    itemType: 1,
+                    imgUrlLrg: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_64.png',
+                    imgUrlSml: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_32.png',
+                    renderUrl: 'https://imageserver.eveonline.com/Render/' + responseItems[item].id + '_256.png'
+                });
+                newShip.save();
+                ships.push(newShip);
+            }
         }
         res.json(ships);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
     });
 };
@@ -180,29 +112,29 @@ exports.updateBargeData = function updateBargeData(req, res) {
 exports.updateExhumerData = function updateExhumerData(req, res) {
     var ships = [];
     var allPromises = [];
-    for(var exhumerId in exhumerIds) {
+    for (var exhumerId in exhumerIds) {
         var options = {
-            uri: eveItemCrestEndpoint + exhumerIds[exhumerId] +'/',
+            uri: eveItemCrestEndpoint + exhumerIds[exhumerId] + '/',
             json: true
         };
         allPromises.push(RequestPromise(options));
     }
-    Promise.all(allPromises).then(function (responseItems){
-        for(var item in responseItems){
+    Promise.all(allPromises).then(function (responseItems) {
+        for (var item in responseItems) {
             var newShip = new shipModel({
-                _id : responseItems[item].id,
+                _id: responseItems[item].id,
                 name: responseItems[item].name,
                 description: responseItems[item].description,
                 itemType: 2,
-                imgUrlLrg: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_64.png',
-                imgUrlSml: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_32.png',
-                renderUrl: 'https://imageserver.eveonline.com/Render/'+responseItems[item].id+'_256.png'
+                imgUrlLrg: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_64.png',
+                imgUrlSml: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_32.png',
+                renderUrl: 'https://imageserver.eveonline.com/Render/' + responseItems[item].id + '_256.png'
             });
             newShip.save();
             ships.push(newShip);
         }
         res.json(ships);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
     });
 };
@@ -210,29 +142,29 @@ exports.updateExhumerData = function updateExhumerData(req, res) {
 exports.updateFrigateData = function updateFrigateData(req, res) {
     var ships = [];
     var allPromises = [];
-    for(var miningFrigateId in miningFrigateIds) {
+    for (var miningFrigateId in miningFrigateIds) {
         var options = {
-            uri: eveItemCrestEndpoint + miningFrigateIds[miningFrigateId] +'/',
+            uri: eveItemCrestEndpoint + miningFrigateIds[miningFrigateId] + '/',
             json: true
         };
         allPromises.push(RequestPromise(options));
     }
-    Promise.all(allPromises).then(function (responseItems){
-        for(var item in responseItems){
+    Promise.all(allPromises).then(function (responseItems) {
+        for (var item in responseItems) {
             var newShip = new shipModel({
-                _id : responseItems[item].id,
+                _id: responseItems[item].id,
                 name: responseItems[item].name,
                 description: responseItems[item].description,
                 itemType: 3,
-                imgUrlLrg: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_64.png',
-                imgUrlSml: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_32.png',
-                renderUrl: 'https://imageserver.eveonline.com/Render/'+responseItems[item].id+'_256.png'
+                imgUrlLrg: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_64.png',
+                imgUrlSml: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_32.png',
+                renderUrl: 'https://imageserver.eveonline.com/Render/' + responseItems[item].id + '_256.png'
             });
             newShip.save();
             ships.push(newShip);
         }
         res.json(ships);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
     });
 };
@@ -240,29 +172,29 @@ exports.updateFrigateData = function updateFrigateData(req, res) {
 exports.updateCommandShipData = function updateCommandShipData(req, res) {
     var ships = [];
     var allPromises = [];
-    for(var industrialCommandShipId in industrialCommandShipIds) {
+    for (var industrialCommandShipId in industrialCommandShipIds) {
         var options = {
-            uri: eveItemCrestEndpoint + industrialCommandShipIds[industrialCommandShipId] +'/',
+            uri: eveItemCrestEndpoint + industrialCommandShipIds[industrialCommandShipId] + '/',
             json: true
         };
         allPromises.push(RequestPromise(options));
     }
-    Promise.all(allPromises).then(function (responseItems){
-        for(var item in responseItems){
+    Promise.all(allPromises).then(function (responseItems) {
+        for (var item in responseItems) {
             var newShip = new shipModel({
-                _id : responseItems[item].id,
+                _id: responseItems[item].id,
                 name: responseItems[item].name,
                 description: responseItems[item].description,
                 itemType: 4,
-                imgUrlLrg: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_64.png',
-                imgUrlSml: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_32.png',
-                renderUrl: 'https://imageserver.eveonline.com/Render/'+responseItems[item].id+'_256.png'
+                imgUrlLrg: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_64.png',
+                imgUrlSml: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_32.png',
+                renderUrl: 'https://imageserver.eveonline.com/Render/' + responseItems[item].id + '_256.png'
             });
             newShip.save();
             ships.push(newShip);
         }
         res.json(ships);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
     });
 };
@@ -271,29 +203,29 @@ exports.updateCommandShipData = function updateCommandShipData(req, res) {
 exports.updateHaulerData = function updateHaulerData(req, res) {
     var ships = [];
     var allPromises = [];
-    for(var haulerShipId in haulerShipIds) {
+    for (var haulerShipId in haulerShipIds) {
         var options = {
-            uri: eveItemCrestEndpoint + haulerShipIds[haulerShipId] +'/',
+            uri: eveItemCrestEndpoint + haulerShipIds[haulerShipId] + '/',
             json: true
         };
         allPromises.push(RequestPromise(options));
     }
-    Promise.all(allPromises).then(function (responseItems){
-        for(var item in responseItems){
+    Promise.all(allPromises).then(function (responseItems) {
+        for (var item in responseItems) {
             var newShip = new shipModel({
-                _id : responseItems[item].id,
+                _id: responseItems[item].id,
                 name: responseItems[item].name,
                 description: responseItems[item].description,
                 itemType: 5,
-                imgUrlLrg: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_64.png',
-                imgUrlSml: 'https://imageserver.eveonline.com/Type/'+responseItems[item].id+'_32.png',
-                renderUrl: 'https://imageserver.eveonline.com/Render/'+responseItems[item].id+'_256.png'
+                imgUrlLrg: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_64.png',
+                imgUrlSml: 'https://imageserver.eveonline.com/Type/' + responseItems[item].id + '_32.png',
+                renderUrl: 'https://imageserver.eveonline.com/Render/' + responseItems[item].id + '_256.png'
             });
             newShip.save();
             ships.push(newShip);
         }
         res.json(ships);
-    }).catch(function(err){
+    }).catch(function (err) {
         console.log(err);
     });
 };
